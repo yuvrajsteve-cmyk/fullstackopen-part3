0654cdf19a0d1009740ed76862f465d2e4b442db
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-23 15:54:54 +0530

    3.4: Phonebook backend step 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -45,6 +45,13 @@ app.get('/api/persons/:id', (request, response) => {
    }
 })
 
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
