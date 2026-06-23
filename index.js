04930de21bdb2d9daecb72facf720438da796b5f
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-23 15:37:21 +0530

    3.3: Phonebook backend step 3

--- index.js
+++ index.js
@@ -34,6 +34,17 @@ app.get('/info', (request, response) => {
     <h1>${new Date()}</h1>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+   if(person) {
+    response.json(person)
+   } else {
+    response.status(404).end()
+   }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
