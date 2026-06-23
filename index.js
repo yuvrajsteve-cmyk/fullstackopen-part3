86318ef41f038ef1a1c41698bb2567b4f2ec6e71
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-23 13:33:42 +0530

    3.2: Phonebook backend step 2

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(`<h1>Phonebook has info for ${persons.length} people</h1> 
+    <h1>${new Date()}</h1>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
