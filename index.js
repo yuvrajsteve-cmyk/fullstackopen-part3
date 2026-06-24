92ca013fb56464fce1098d2efc5ef7f6438ceb50
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-24 12:00:38 +0530

    3.8*: Phonebook backend step 8

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
 
 let persons = [
   { 
@@ -27,6 +27,12 @@ let persons = [
     }
   
 ]
+
+morgan.token('body', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
