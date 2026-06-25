e20be881d3abeb7cbed3a159d5d2494050c47a4e
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-25 08:16:31 +0530

    feat: completed full stack deployment 3.11

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require('path')
 app.use(cors())
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static('dist'))
 // app.use(morgan('tiny'))
 
