d27b899e8dc93bfe5b0c92767d57108bff98d2b1
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-25 06:14:01 +0530

    preactice erxercise for npm build

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 // app.use(morgan('tiny'))
 
