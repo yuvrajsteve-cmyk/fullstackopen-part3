5710d775a43627e7762cc2a6bcde9fced706b35d
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-25 07:17:53 +0530

    add frontend production build and static middleware and with this 3.11 Full Stack Phonebook complete

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-app.use(express.static('dist'))
+
 app.use(express.json())
+app.use(express.static('dist'))
 // app.use(morgan('tiny'))
 
 let persons = [
