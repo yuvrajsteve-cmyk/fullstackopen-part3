6c144d8cdccbba41371de6287372c259191bdf80
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-27 13:17:53 +0530

    3.13: Phonebook database, step 1

--- index.js
+++ index.js
@@ -1,105 +1,44 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
-const path = require('path')
+const Person = require(`./models/Persons`)
 
 
-app.use(cors())
-
+app.use(express.static('build'))
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static('dist'))
-// app.use(morgan('tiny'))
-
-let persons = [
-  { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-  
-]
-
-morgan.token('body', (request, response) => {
-  return JSON.stringify(request.body)
-})
-
-app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.get('/info', (request, response) => {
-  response.send(`<h1>Phonebook has info for ${persons.length} people</h1> 
-    <h1>${new Date()}</h1>`)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-   if(person) {
-    response.json(person)
-   } else {
-    response.status(404).end()
-   }
-})
 
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
-})
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+const mongoose =  require('mongoose')
+const url = process.env.MONGO_URI
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: `name or number is missing`
+console.log(`connecting to the ${url}`)
+
+
+
+mongoose.connect(url)
+  .then(result => {
+    console.log(`connected to mongoDB`)
+  })
+  .catch((error) => {
+    console.log(`error to connecting to mongoDB`, error.message)
+  })
+
+
+
+
+  app.get('/api/persons', (request, response) => {
+    Person.find({}).then(people => {
+        response.json(people)
     })
-  }
+  })
+   
 
-  const nameExists = persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())
 
-  if (nameExists) {
-    return response.status(400).json({
-      error: `name must be unique`
-    })
-  }
 
-  const randomId = Math.floor(Math.random() * 1000000)
 
-  const person = {
-    id: String(randomId),
-    name: body.name,
-    number: body.number
-  }
-
-  persons = persons.concat(person)
-
-  response.status(201).json(person)
-})
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`)
-})
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+  })
