d50733427da05eca04ed9ccf25a6d77101718099
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-29 16:37:10 +0530

    doing some changes

--- models/Persons.js
+++ models/Persons.js
@@ -12,7 +12,7 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
        validator: function(v) {
-        return /^\d{2,3}-\d+$/.test(v);
+        return /^\d{2,3}-?\d+$/.test(v);
        },
        message: props => `${props.value} is not vaild phone number`
     }
